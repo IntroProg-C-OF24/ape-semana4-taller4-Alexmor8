ffc57eb351d392c8e3e8fd274b58d8da46aa0d5a
MArio Morocho <mamorocho33@utpl.edu.ec>
2023-11-01 16:25:18 -0500

    Primer ejercico

---
+++ Ejercicio01/src/main/java/com/mycompany/ejercicio01/Ejercicio01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio01;
+
+/**
+ *
+ * @author SALAI
+ */
+public class Ejercicio01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
